2aa4c2b6ea46c8f2e5ab5e7f04060301be3f48ed
Joro2003 <talking2myselfsmra2003@gmail.com>
2025-04-07 17:36:39 +0600

    Update Delete_at_head.java

--- Module 7/Delete_at_head.java
+++ Module 7/Delete_at_head.java
@@ -1,67 +1,65 @@
-#include<bits/stdc++.h>
-using namespace std;
+import java.util.Scanner;
 
-class Node
-{
-    public:
-        int val;
-        Node* next;
-    Node(int val)
-    {
-        this->val = val;
-        this->next = NULL;
-    }
-};
-
-void insert_at_tail(Node* &head, Node* &tail, int val)
-{
-    Node *newnode = new Node(val);
-    if (head == NULL)
-    {
-        head = newnode;
-        tail = newnode;
-        return;
-    }
-    tail->next = newnode;
-    tail = newnode;
-}
-
-void print_linked_list(Node* head)
-{
-    Node* tmp = head;
-    while(tmp != NULL)
-    {
-        cout << tmp->val << endl;
-        tmp = tmp->next;
-    }
-}
-
-void delete_at_head(Node* &head)
-{
-    if (head == NULL) return; // Edge case: if the list is empty, do nothing
-    Node* deleteNode = head;
-    head = head->next;
-    delete deleteNode;
-}
-
-int main()
-{
-    Node* head = NULL;
-    Node* tail = NULL;
-
+class Node {
     int val;
-    while (true)
-    {
-        cin >> val;
-        if (val == -1)
-        {
-            break;
-        }
-        insert_at_tail(head, tail, val);
-    }
-    
-    delete_at_head(head); // Deleting the head node
-    print_linked_list(head); // Printing the updated list after deletion
+    Node next;
 
-    return 0;
+    // Constructor for Node
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+    }
 }
+
+public class Main {
+
+    // Method to insert a node at the tail
+    public static void insertAtTail(Node head, Node tail, int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    // Method to print the linked list
+    public static void printLinkedList(Node head) {
+        Node tmp = head;
+        while (tmp != null) {
+            System.out.println(tmp.val);
+            tmp = tmp.next;
+        }
+    }
+
+    // Method to delete the head node
+    public static void deleteAtHead(Node head) {
+        if (head == null) return; // Edge case: if the list is empty, do nothing
+        Node deleteNode = head;
+        head = head.next;
+        deleteNode = null; // Set the deleted node to null (in Java, the garbage collector will handle memory)
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        Node head = null;
+        Node tail = null;
+        
+        int val;
+        
+        while (true) {
+            val = sc.nextInt();
+            if (val == -1) {
+                break;
+            }
+            insertAtTail(head, tail, val);
+        }
+        
+        deleteAtHead(head); // Deleting the head node
+        printLinkedList(head); // Printing the updated list after deletion
+    }
+}
+
